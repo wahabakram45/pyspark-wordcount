67c96e94fb8c524e993bff31363ffd3a07d33da7
Abdul Wahab Akram <abdul.wahab.1385@slashnext.com>
2020-03-23 23:26:54 +0500

    updation

--- word_count.py
+++ word_count.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
 	sc = SparkContext("local","PySpark Word Count Exmaple")
 	
 	# read data from text file and split each line into words
-	words = sc.textFile("input_file.txt").flatMap(lambda line: line.split(" "))
+	words = sc.textFile("gs://crafty-sound-270719/pyspark-wordcount/input_file.txt").flatMap(lambda line: line.split(" "))
 	
 	# count the occurrence of each word
 	wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
 	
 	# save the counts to output
-	wordCounts.saveAsTextFile("output/")
+	wordCounts.saveAsTextFile("gs://crafty-sound-270719/pyspark-wordcount/output/")
